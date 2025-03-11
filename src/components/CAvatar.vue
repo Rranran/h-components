c50e8882bd6a79dadbb2cd4f1a0ac92c455b80cc
Rranran <rranran@qq.com>
2025-03-11 22:14:02 +0800

    feat: 样式修改

--- src/components/CAvatar.vue
+++ src/components/CAvatar.vue
@@ -12,14 +12,18 @@
   <a-modal wrapClassName="avatar-upload-modal" v-model:open="uploadOpen" title="头像上传" :keyboard="false"
     :mask-closable="false" :width="972" :ok-button-props="{ type: 'primary' }" @ok="confirmUpload">
     <div class="wrapper-box">
+      <input ref="uploadRef" type="file" style="display: none;" @change="beforeUpload"></input>
       <div class="left">
         <div class="crop-box" v-if="currentImg">
+          <div class="btns">
+            <a-button type="primary" @click="clickUpload">重新上传</a-button>
+            <a-button danger style="margin-left: 8px;" @click="clickDeleteCurrent">删除</a-button>
+          </div>
           <vueCropper ref="cropperRef" :auto-crop-width="`${width}px`" :auto-crop-height="`${height}px`" :fixed="false"
             :img="currentImg" :fixed-box="true" :auto-crop="true" :center-box="false" output-type="png" :info="false"
             :can-move-box="false" @realTime="handleRealTime" />
         </div>
         <div v-else class="upload-box">
-          <input ref="uploadRef" type="file" style="display: none;" @change="beforeUpload"></input>
           <img src="../assets/images/picture.png">
           <a-button type="link" @click="clickUpload" style="margin-top:12px">
             <template #icon>
@@ -33,9 +37,12 @@
         </div>
       </div>
       <div class="preview-box">
-        <div :style="`height: ${height}px;width:${width}px`">
-          <span v-html="html"></span>
+        <div :style="`height: ${height}px;width:${width}px;background: #F0F0F0;overflow: hidden;`">
+          <img v-if="!!previewObj" :src="previewObj.url" :style="previewObj.img" class="preview-img">
         </div>
+        <div class="tip">{{ width }}*{{ height }}</div>
+        <a-button type="primary" ghost style="margin-top:20px" :disabled="!currentImg"
+          @click="downloadPng">下载图片</a-button>
       </div>
     </div>
   </a-modal>
@@ -59,9 +66,14 @@
     accept: { type: Array, default: () => ['image/jpeg', 'image/jpg', 'image/png'] }
   });
 
+  const validTip = computed(() => {
+    const { accept, limit } = props
+    return `大小不得超过${limit}M,仅支持${accept.map((v) => v.split('/')[1].toUpperCase()).join('/')}`
+  })
+
   const tip = computed(() => {
-    const { height, width, accept, limit } = props
-    return `尺寸为${width}*${height},大小不得超过${limit}M,仅支持${accept.map((v) => v.split('/')[1].toUpperCase()).join('/')}`
+    const { height, width } = props
+    return `尺寸为${width}*${height},${validTip.value}}`
   })
 
   const emits = defineEmits(['update:modelValue']);
@@ -72,12 +84,15 @@
   function clickOpen() {
     uploadOpen.value = true;
     currentImg.value = props.modelValue;
+    previewObj.value = {
+      url: props.modelValue,
+      img: ''
+    }
   }
 
   function clickDelete() {
     emits('update:modelValue', '');
   }
-
   const uploadRef = ref(null);
   function clickUpload() {
     uploadRef.value.click();
@@ -86,12 +101,17 @@
     const file = e.target.files[0];
     const { type, size } = file;
     if (!['image/jpeg', 'image/jpg', 'image/png'].includes(type) || size > 5 * 1024 * 1024) {
-      message.warning('仅支持JPG、PNG、JPEG，大小不超过5M');
+      message.warning(validTip.value);
       return;
     }
     const reader = new FileReader();
     reader.onload = function (e) {
       currentImg.value = e.target.result;
+      uploadRef.value.value = ''
+    };
+    reader.onerror = function (e) {
+      message.error('文件读取失败，请检查是否被占用');
+      uploadRef.value.value = ''
     };
     reader.readAsDataURL(file);
   }
@@ -103,10 +123,24 @@
       uploadOpen.value = false;
     });
   }
-  const html = ref('')
+  const previewObj = ref('')
   function handleRealTime(v) {
-    console.log(v)
-    html.value = v.html
+    previewObj.value = v
+  }
+  function clickDeleteCurrent() {
+    currentImg.value = ''
+    previewObj.value = ''
+  }
+  function downloadPng() {
+    cropperRef.value.getCropBlob((data) => {
+      const href = URL.createObjectURL(data)
+      const link = document.createElement('a')
+      link.download = '头像.png'
+      link.href = href;
+      link.click()
+      document.removeChild(link)
+      URL.revokeObjectURL(href)
+    });
   }
 </script>
 
@@ -134,39 +168,41 @@
     width: 100%;
     display: block;
   }
-}
 
-.btns {
-  display: none;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  height: 24px;
-  background: rgba(0, 130, 255, 0.6);
-  width: 100%;
-  z-index: 1;
-  line-height: 24px;
+  .btns {
+    display: none;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 24px;
+    background: rgba(0, 130, 255, 0.6);
+    width: 100%;
+    z-index: 1;
+    line-height: 24px;
 
-  .btn {
-    min-width: 0;
-    flex: 1;
-    font-family: PingFangSC;
-    font-size: 12px;
-    color: #FFFFFF;
-    text-align: center;
-    cursor: pointer;
-    position: relative;
-
-    +.btn::before {
-      display: inline-block;
-      content: '|';
-      font-size: 14px;
-      position: absolute;
-      left: 0;
+    .btn {
+      min-width: 0;
+      flex: 1;
+      font-family: PingFangSC;
+      font-size: 12px;
       color: #FFFFFF;
+      text-align: center;
+      cursor: pointer;
+      position: relative;
+
+      +.btn::before {
+        display: inline-block;
+        content: '|';
+        font-size: 14px;
+        position: absolute;
+        left: 0;
+        color: #FFFFFF;
+      }
     }
   }
 }
+
+
 
 
 .wrapper-box {
@@ -178,25 +214,37 @@
     min-width: 0;
     flex: 1;
 
-    .tip {
-      font-size: 14px;
-      color: #666666;
-      line-height: 20px;
-      margin-top: 16px;
-    }
   }
 
   .preview-box {
     margin-left: 32px;
-    width: 280px;
-    height: 360px;
-    background: #F0F0F0;
     flex: none;
+    text-align: center;
+
+    .preview-img {
+      display: block;
+    }
   }
+}
+
+.tip {
+  font-size: 14px;
+  color: #666666;
+  line-height: 20px;
+  margin-top: 16px;
 }
 
 .crop-box {
   height: 450px;
+  position: relative;
+
+  .btns {
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    z-index: 20;
+    display: flex;
+  }
 
   :deep(.vue-cropper) {
     background-image: none;
